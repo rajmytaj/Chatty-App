b7f1ebadb72b6bd5b0d0056403a8262998a4dba6
rajmytaj <chalasanidheeraj@gmail.com>
2017-07-27 00:47:04 +0000

    chat works on multiple sessions at the same time

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,7 +1,10 @@
 // server.js
 
-const express      = require('express');
+const express = require('express');
 const SocketServer = require('ws').Server;
+const uuidv4 = require('uuid/v4');
+const WebSocket = require('ws');
+
 
 // Set port to 3001
 const PORT = 3001;
@@ -11,7 +14,7 @@ const PORT = 3001;
 const server = express()
   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
 // Create the WebSockets server
 const wss = new SocketServer({ server: server });
@@ -22,11 +25,16 @@ const wss = new SocketServer({ server: server });
 
 wss.on('connection', (ws) => {
   console.log('Client Connected');
-  ws.send('connected to server')
   ws.on('message', function incoming(data) {
-    console.log('User', JSON.parse(data).username, 'said', JSON.parse(data).content);
+    const msgInfo = (JSON.parse(data));
+    msgInfo['id'] = uuidv4();
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(msgInfo));
+      }
+    })
+    // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+    ws.on('close', () => console.log('Client disconected'));
   });
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconected')); 
-});
+})
 
